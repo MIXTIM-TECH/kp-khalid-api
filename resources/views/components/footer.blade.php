2ce5a645fd99df50d42b193f90bc588cb26c031f
afaris01 <sidukertas0@gmail.com>
2023-05-24 12:14:22 +0700

    landingpage

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,37 @@
-<footer>Footer</footer>
+<footer>
+    <div class="container-fluid mt-5">
+        <div class="row p-4 bg-primary">
+        </div>
+        <div class="footer-content md">
+            <div class="row">
+                <div class="col-md-8 col-sm-12">
+                    <img src="" alt="">
+                    <a href=""><h6>Polandu JK</h6></a>
+                    <p>Polandu JK adalah website untuk masyarakat Kelurahan Jembatan Kecil yang memungkinkan
+                        pengajuan surat-surat secara daring, seperti surat keterangan tidak mampu dan surat pengantar
+                        perkawinan.</p>
+                </div>
+                <div class="col-md-4 col-sm-12 text-end">
+                    <img src="" alt="">
+                    <h6>Kontak Kami</h6>
+                    <a href=""><i>Email</i></a>
+                    <a href=""><i>Telepon</i></a>
+                    <a href=""><i>Instagram</i></a>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <h6>Ajukan Surat</h6>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <p>Hanya dengan sentuhan jari dengan POLANDU JK</p>
+                </div>
+
+                <div class="col-md-6 col-sm-12 text-end">
+                    <p>&copy; 2023 Polandu JK. All Right Reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
