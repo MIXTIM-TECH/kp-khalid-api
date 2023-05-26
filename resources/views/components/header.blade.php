c7ff95f6e5708a33e6829fcf65106ab4614ea79d
afaris01 <sidukertas0@gmail.com>
2023-05-26 15:27:20 +0700

    updatepage

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,32 @@
-<header>Header</header>
+<body>
+    <header>
+        <nav class="navbar bg-white navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid md p-0">
+                <a class="navbar-brand" href="#">Polandu</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#beranda">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#tentang-kami">Tentang Kami</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#hubungi-kami">Hubungi Kami</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
+                            data-bs-target="#login-form">
+                            Log In
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
